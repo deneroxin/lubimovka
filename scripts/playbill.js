851c335ee393510e16a393d5c9ab4a40bc7e83ab
Denis Eroxin <deneroxin@yandex.ru>
2022-10-22 23:22:21 +0300

    feat: добавить общий стиль заголовка
    1) Добавлен общий стиль заголовка - section-title
    2) Добавлен общий стиль кнопки и прикручен к кнопке "Проекты" - button
    3) Добавлен стиль ссылки, добавляющий скобки по бокам - nav-link
    (чтобы протестировать, класс был добавлен в 4 ссылки header-а
    и в первую колонку footer-а)

--- scripts/playbill.js
+++ scripts/playbill.js
@@ -1,7 +1,2 @@
-// Скопировать текст каждой кнопки в свойство "content" её псевдо-элемента ::before,
-// с помощью которого выполнена анимация кнопки и требует дублирования текста кнопки
-const allTheButtons = Array.from(document.querySelectorAll('.button_type_left'));
-allTheButtons.forEach(button => button.setAttribute('data-before', button.textContent));
-
 const playbillSliders = Array.from(document.querySelectorAll('.playbill__slider'));
-playbillSliders.forEach(el => new CustomSlider(el, 'button_type_left'));
+playbillSliders.forEach(el => new CustomSlider(el, 'button'));
--- scripts/project-page.js
+++ scripts/project-page.js
@@ -1,1 +1,6 @@
 console.log('Script attached');
+
+// Скопировать текст каждой кнопки в свойство "content" её псевдо-элемента ::before,
+// с помощью которого выполнена анимация кнопки и требует дублирования текста кнопки
+const allTheButtons = Array.from(document.querySelectorAll('.button'));
+allTheButtons.forEach(button => button.setAttribute('data-before', button.textContent));
